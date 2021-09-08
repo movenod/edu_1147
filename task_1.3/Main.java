5defdd2c910e80419ea9b1c16e92bff6c4eb7c57
movenod <88160029+movenod@users.noreply.github.com>
2021-09-08 19:37:59 +0200

    Update Main.java

--- task_1.3/Main.java
+++ task_1.3/Main.java
@@ -1,1 +1,12 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
 
+public class Main {
+public static void main(String[] args) {
+int a =5; int b = 7; String s = "Вологда";
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(s);
+}
+}
