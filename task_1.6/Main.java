832921e60d704197f4c993fe7515aeb8eba31777
movenod <88160029+movenod@users.noreply.github.com>
2021-09-08 20:44:10 +0200

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите любое число:");
+
+
+        Scanner scanner = new Scanner(System.in);
+        float fA = scanner.nextFloat();
+        fA = fA + (fA * 15 / 100);
+        System.out.println("Значение числа, увеличенное на 15% равно " + fA);
+
+    }
+}
