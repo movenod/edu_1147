86706360921106de5a45f0b94038061604180db3
movenod <88160029+movenod@users.noreply.github.com>
2021-09-09 22:25:42 +0200

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -17,9 +17,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 1; i < 12; i++){
+         for(int i = 1; i <= 11; i++){
             for(int j = 1; j < i; j++){
-              System.out.print("8");
+                System.out.print("8");
             }
             System.out.println();
         }
