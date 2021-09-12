d38d050aeade214edbd0b0a8054dde0422ecb37e
movenod <88160029+movenod@users.noreply.github.com>
2021-09-12 22:30:24 +0200

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -1,1 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
 
+
+
+        int[] nums = {2,3,4,5};
+        int mult = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            mult=mult*nums[i];
+            //System.out.println(mult);
+
+            }
+        System.out.println(mult);
+        }
+
+    }
