5f1be7e82217c0163450284e39c15c2f2f2d1ad7
movenod <88160029+movenod@users.noreply.github.com>
2021-09-18 18:52:00 +0200

    Update Main.java

--- task_2.7/Main.java
+++ task_2.7/Main.java
@@ -1,1 +1,25 @@
+/*
+Задание: Дано число, например 31. Проверьте, что это число не делится
+ ни на одно другое число кроме себя самого и единицы.
+ То есть в нашем случае нужно проверить, что число 31 не делится на все числа от 2 до 30.
+ Если число не делится - выведите 'false', а если делится - выведите 'true'.
+*/
 
+public class Main {
+    public static void main(String[] args) {
+
+        int num =31;
+
+        for ( int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                //System.out.println(" Это число не простое = "+i+" "+num);
+                System.out.println("true");
+                return;
+            }
+            //System.out.println(i); // Смотрел как цикл работает.
+        }
+//System.out.println("Поздравляем! Число простое. "+num);
+        System.out.println("false");
+    }
+
+}
