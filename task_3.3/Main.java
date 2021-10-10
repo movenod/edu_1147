73fa0aea0974cf8547d487d1e99f70509eb412ae
movenod <88160029+movenod@users.noreply.github.com>
2021-10-11 00:49:21 +0200

    Create Main.java

---
+++ task_3.3/Main.java
@@ -0,0 +1,16 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа.
+ Если эти числа равны - пусть функция вернет true, а если не равны - false.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(compare(5, 5));
+    }
+    public static boolean compare(int a, int b) {
+        return a == b;
+
+    }
+}
