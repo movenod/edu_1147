e13c8e13878b9b5c6b6f8d8abdeebafe7831a5e4
movenod <88160029+movenod@users.noreply.github.com>
2021-10-11 07:40:33 +0200

    Create Main.java

---
+++ task_4.2/Main.java
@@ -0,0 +1,43 @@
+/*
+Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+Создайте геттеры и сеттеры для всех переменных класса Dog.
+Требования:
+•	Программа не должна считывать данные с клавиатуры.
+•	У класса Dog должна быть переменная name с типом String.
+•	У класса Dog должна быть переменная age с типом int.
+•	У класса должен быть сеттер для переменной name.
+•	У класса должен быть геттер для переменной name.
+•	У класса должен быть сеттер для переменной age.
+•	У класса должен быть геттер для переменной age.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.setName("Rexi");
+        myDog.setAge(7);
+        System.out.println("Кличка собаки: " + myDog.getName() + ", ее возраст: " + myDog.getAge());
+    }
+}
+
+class Dog {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
